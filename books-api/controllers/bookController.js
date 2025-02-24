4e966d453bd2e3c09e934e8c0bc6eba320eac2fa
3214saiteja <saitejasriram3214@gmail.com>
2025-02-24 17:16:20 +0530

    adding postman_collection_json and add instruction to the project for implementation

--- books-api/controllers/bookController.js
+++ books-api/controllers/bookController.js
@@ -1,14 +1,5 @@
 const Book = require('../models/bookModel');
 
-// Get all books
-// exports.getBooks = async (req, res) => {
-//   try {
-//     const books = await Book.find();
-//     res.json(books);
-//   } catch (error) {
-//     res.status(500).json({ message: 'Server error', error });
-//   }
-// };
 
 exports.getBooks = async (req, res) => {
   try {
@@ -23,16 +14,6 @@ exports.getBooks = async (req, res) => {
 
 
 
-// Get a single book by ID
-// exports.getBookById = async (req, res) => {
-//   try {
-//     const book = await Book.findById(req.params.id);
-//     if (!book) return res.status(404).json({ message: 'Book not found' });
-//     res.json(book);
-//   } catch (error) {
-//     res.status(500).json({ message: 'Server error', error });
-//   }
-// };
 
 
 exports.getBookById = async (req, res) => {
@@ -46,21 +27,6 @@ exports.getBookById = async (req, res) => {
 };
 
 
-// Create a new book
-// exports.createBook = async (req, res) => {
-//   const { title, author, publishedDate, genre } = req.body;
-//   if (!title || !author || !publishedDate || !genre) {
-//     return res.status(400).json({ message: 'All fields are required' });
-//   }
-
-//   try {
-//     const newBook = new Book({ title, author, publishedDate, genre });
-//     await newBook.save();
-//     res.status(201).json(newBook);
-//   } catch (error) {
-//     res.status(500).json({ message: 'Failed to create book', error });
-//   }
-// };
 
 
 exports.createBook = async (req, res) => {
@@ -79,16 +45,7 @@ exports.createBook = async (req, res) => {
 };
 
 
-// Update a book
-// exports.updateBook = async (req, res) => {
-//   try {
-//     const book = await Book.findByIdAndUpdate(req.params.id, req.body, { new: true });
-//     if (!book) return res.status(404).json({ message: 'Book not found' });
-//     res.json(book);
-//   } catch (error) {
-//     res.status(500).json({ message: 'Failed to update book', error });
-//   }
-// };
+
 
 
 
@@ -107,16 +64,6 @@ exports.updateBook = async (req, res) => {
 };
 
 
-// Delete a book
-// exports.deleteBook = async (req, res) => {
-//   try {
-//     const book = await Book.findByIdAndDelete(req.params.id);
-//     if (!book) return res.status(404).json({ message: 'Book not found' });
-//     res.json({ message: 'Book deleted successfully' });
-//   } catch (error) {
-//     res.status(500).json({ message: 'Failed to delete book', error });
-//   }
-// };
 
 
 exports.deleteBook = async (req, res) => {
